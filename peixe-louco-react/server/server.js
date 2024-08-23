01335425fd1d833731eb46cc869ab75674d8f3f2
laisdiasdev <laisgabrielyds@gmail.com>
2024-08-22 21:22:46 -0300

    lógica do servidor

---
+++ peixe-louco-react/server/server.js
@@ -0,0 +1,20 @@
+// Deixando só o necessário
+const express = require("express");
+const cors = require('cors');
+
+const userRoutes = require('./routes/userRoutes');
+const db = require('./config/db');
+
+const app = express();
+
+// Middleware
+app.use(express.json()); // Processa requisição JSON
+app.use(cors())
+
+// Rota de usuários
+app.use('/api', userRoutes)
+
+// Porta do servidor 
+app.listen(3000, () => {
+  console.log('Servidor ok')
+});
